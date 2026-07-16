4e2a929f4e554ca6d465c3a85edf82bda6c47223
shinjh <shinjuhyean18@gmail.com>
2026-07-16 15:11:09 +0900

    Backend Book

---
+++ LibraryBackend/src/test/java/com/library/book/dto/BookDTO.java
@@ -0,0 +1,21 @@
+package com.library.book.dto;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class BookDTO {
+	private int bookNumber;
+	private int bookNumberInfo;
+	private String bookImg;
+	private String bookTitle;
+	private String 	Writer;
+	private String Publisher;
+	private String 	Category;
+	private String 	bookContent;
+	private boolean loanAvailable;
+	private Date publicationDate;
+	private Date registrationDate;
+
+}
---
+++ LibraryBackend/src/test/java/com/library/book/mapper/BookMapper.java
@@ -0,0 +1,11 @@
+package com.library.book.mapper;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+	
+@Mapper
+public interface BookMapper {
+	
+	List<String>BookList();
+		
+}
