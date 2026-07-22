33d827979065af886c662744239685fe1aaec5bb
sj.park <psj1197@naver.com>
2026-07-22 11:54:08 +0900

    イメージ登録設定

--- LibraryBackend/src/main/java/com/library/WebConfig.java
+++ LibraryBackend/src/main/java/com/library/WebConfig.java
@@ -3,22 +3,29 @@ package com.library;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
 
-    @Bean
-    WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
+	@Bean
+	WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:5173")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+
+			public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+				registry.addResourceHandler("/uploads/**")
+						.addResourceLocations("file:///C:/uploads/images/");
+			}
+		};
+	}
 }
