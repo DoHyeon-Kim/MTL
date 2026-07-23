fa8b0e5520d0975b4b9c14d8e59cd005791e93ef
sj.park <psj1197@naver.com>
2026-07-23 11:14:14 +0900

    router 設定

--- LibraryFrontend/src/router/index.ts
+++ LibraryFrontend/src/router/index.ts
@@ -2,61 +2,64 @@ import { createRouter, createWebHistory } from "vue-router";
 import { mainRoutes } from "./mainRoutes";
 import { managementRoutes } from "./management";
 
-import member from './member'
-import user from './user'
-import admin from './admin'
+import member from "./member";
+import user from "./user";
+import admin from "./admin";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
 
-  routes: [...mainRoutes, ...managementRoutes,  ...member,
-  ...user,
-  ...admin,], 
+  routes: [
+    ...mainRoutes,
+    ...managementRoutes,
+    ...member,
+    ...user,
+    ...admin,
     {
-      path: '/announcement',
-      name: 'announcement-list',
-      component: () => import('@/views/announcement/AnnouncementListView.vue'),
+      path: "/announcement",
+      name: "announcement-list",
+      component: () => import("@/views/announcement/AnnouncementListView.vue"),
       meta: { showMainHeader: true, showMainSidebar: false, showMainFooter: true },
     },
     {
-      path: '/announcement/new',
-      name: 'announcement-new',
-      component: () => import('@/views/announcement/AnnouncementFormView.vue'),
+      path: "/announcement/new",
+      name: "announcement-new",
+      component: () => import("@/views/announcement/AnnouncementFormView.vue"),
       meta: { showMainHeader: true, showMainSidebar: false, showMainFooter: true },
     },
     {
-      path: '/announcement/:id/edit',
-      name: 'announcement-edit',
-      component: () => import('@/views/announcement/AnnouncementFormView.vue'),
+      path: "/announcement/:id/edit",
+      name: "announcement-edit",
+      component: () => import("@/views/announcement/AnnouncementFormView.vue"),
       meta: { showMainHeader: true, showMainSidebar: false, showMainFooter: true },
     },
     {
-      path: '/announcement/:id',
-      name: 'announcement-detail',
-      component: () => import('@/views/announcement/AnnouncementDetailView.vue'),
+      path: "/announcement/:id",
+      name: "announcement-detail",
+      component: () => import("@/views/announcement/AnnouncementDetailView.vue"),
       meta: { showMainHeader: true, showMainSidebar: false, showMainFooter: true },
     },
 
     // Q&A
     {
-      path: '/qanda',
-      name: 'qanda-list',
-      component: () => import('@/views/qanda/QandaListView.vue'),
+      path: "/qanda",
+      name: "qanda-list",
+      component: () => import("@/views/qanda/QandaListView.vue"),
       meta: { showMainHeader: true, showMainSidebar: false, showMainFooter: true },
     },
     {
-      path: '/qanda/new',
-      name: 'qanda-new',
-      component: () => import('@/views/qanda/QandaFormView.vue'),
+      path: "/qanda/new",
+      name: "qanda-new",
+      component: () => import("@/views/qanda/QandaFormView.vue"),
       meta: { showMainHeader: true, showMainSidebar: false, showMainFooter: true },
     },
     {
-      path: '/qanda/:id',
-      name: 'qanda-detail',
-      component: () => import('@/views/qanda/QandaFormView.vue'),
+      path: "/qanda/:id",
+      name: "qanda-detail",
+      component: () => import("@/views/qanda/QandaFormView.vue"),
       meta: { showMainHeader: true, showMainSidebar: false, showMainFooter: true },
     },
   ],
 });
 
-export default router
+export default router;
